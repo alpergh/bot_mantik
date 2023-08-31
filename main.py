89876542cb2f33b342011138c4115180c907de03
alpergh <141397256+alpergh@users.noreply.github.com>
2023-08-31 21:22:15 +0300

    Update main.py
    clienttan bota geçiş

--- main.py
+++ main.py
@@ -1,28 +1,21 @@
 import discord
-from bot_mantik import sifre_uret
+from discord.ext import commands
 
-# ayricaliklar (intents) değişkeni botun ayrıcalıklarını depolayacak
 intents = discord.Intents.default()
-# Mesajları okuma ayrıcalığını etkinleştirelim
 intents.message_content = True
-# client (istemci) değişkeniyle bir bot oluşturalım ve ayrıcalıkları ona aktaralım
-client = discord.Client(intents=intents)
 
-@client.event
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'{bot.user} olarak giriş yaptık')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send("Hi!")
-    if message.content.startswith('$password'):
-        await message.channel.send(sifre_uret(10))
-    elif message.content.startswith('$bye'):
-        await message.channel.send("\U0001f642")
-    else:
-        await message.channel.send(message.content)
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba {bot.user}! Ben bir botum!')
 
-client.run("your token put")
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+bot.run("MTE0NDMyOTg3NDEzNjM3MTM0MQ.GBaR-W.sDeiNJNhnzivCGoNRs5etmBU9XwbMVTCGnN-Lw")
